1ddfeb793148ca11ceeadaa784fa7674f377da63
janhnguyen <nguyenjanh01@gmail.com>
2026-02-09 17:49:37 -0500

    v5

--- medium/longestSubstringWithoutRepeatingCharacters.py
+++ medium/longestSubstringWithoutRepeatingCharacters.py
@@ -1,24 +1,23 @@
-def lengthOfLongestSubstring(s):
-    """
-    :type s: str
-    :rtype: int
-    """
-    if len(s) < 2 :
-        return len(s)
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s) < 2 :
+            return len(s)
 
-    maxVal = 0
-    chars = set()
-    x = 0
+        maxVal = 0
+        chars = set()
+        x = 0
 
-    for y in range(len(s)) :
+        for y in range(len(s)) :
 
-        while s[y] in chars :
-            chars.remove(s[x])
-            x += 1
+            while s[y] in chars :
+                chars.remove(s[x])
+                x += 1
 
-        chars.add(s[y])
-        maxVal = max(maxVal, len(chars))
+            chars.add(s[y])
+            maxVal = max(maxVal, len(chars))
 
-    return maxVal
-
-print(lengthOfLongestSubstring("abcdefgh"))
+        return maxVal

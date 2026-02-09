86341bc685969efcf91ff9d7afd683b3fb59db80
janhnguyen <nguyenjanh01@gmail.com>
2026-02-09 13:18:34 -0500

    v3

--- medium/longestSubstringWithoutRepeatingCharacters.py
+++ medium/longestSubstringWithoutRepeatingCharacters.py
@@ -3,11 +3,22 @@ def lengthOfLongestSubstring(s):
     :type s: str
     :rtype: int
     """
+    if len(s) < 2 :
+        return len(s)
+
     maxVal = 0
-    for x in range(len(s)) :
-        for y in range(x + 1, len(s)) :
-            if s[x] == s[y] :
-                if len(s[x:y]) > maxVal :
-                    maxVal = len(s[x:y])
-                break
+    chars = set()
+    x = 0
+
+    for y in range(len(s)) :
+
+        while s[y] in chars :
+            chars.remove(s[x])
+            x += 1
+
+        chars.add(s[y])
+        maxVal = max(maxVal, len(chars))
+
     return maxVal
+
+print(lengthOfLongestSubstring("abcdefgh"))
